d4bea12935eab883c91f2e3df9cb2b3435106acb
roktimashraful <roktimashraful4@gmail.com>
2024-01-22 20:22:24 +0600

    day05

---
+++ Os/main.py
@@ -0,0 +1,27 @@
+import os
+
+# os.mkdir("data") 
+
+" 100 folder in data folder "
+
+# for i  in range(1,101):
+#     os.mkdir('tutorial '+str(i))
+    # os.rename("tutorial "+str(i), 'tutorials '+str(i))
+    # os.rmdir("tutorials "+str(i))
+
+folders = os.listdir() 
+
+'''
+[
+""
+    
+]
+
+'''
+
+# print(folders)
+
+# for i in folders: 
+#     print(i)
+
+print(os.getcwd()) # there 
